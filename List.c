51ad6f12270c1df6cfac92fef71f2c04d50cdb6f
Vadim200116 <56249129+Vadim200116@users.noreply.github.com>
2020-09-17 19:03:49 +0300

    Create List.c

---
+++ List.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct sNode{
+   struct sNode* next;
+   int data;
+}tNode;
+
+void push(int data,tNode** head);
+int pop(tNode** head);
+void showall(tNode* head);
+
+
+int main(void){
+    tNode* head=NULL;
+    int log=0;
+    int a=0;
+    int num=0;
+    int ind;
+    while(!log){
+        printf("Press 1 to push a new number\n");
+        printf("Press 2 to pop the last number\n");
+        printf("Press any other symbol to quit\n");
+        showall(head);
+        printf(">");
+        scanf("%d",&a);
+        switch (a){
+            case 1:
+            printf("Enter one number\n");
+            printf(">");
+            scanf("%d",&num);
+            push(num,&head);
+            break;
+        case 2:
+            ind=pop(&head);
+            if(a)
+                printf("Failed to pop the last number\n");
+            else
+                printf("The last number: %d\n",ind);
+            break;
+        default:
+        log=1;
+        break;   
+         }
+    }
+    return 0;
+}
+
+
+
+void showall(tNode* head){
+    if(!head){
+         printf("\t\tList is empty\n"); 
+         return;
+         }
+    printf("\t\tThat's our list\n");   
+    while(head){   
+    printf("\t\t%d\n",head->data);
+    head=head->next;
+    }
+}
+
+void push(int data,tNode** head){
+   tNode* temp=(tNode*)malloc(sizeof(tNode));
+   temp->data=data;
+   temp->next=(*head);
+   (*head)=temp;
+}
+
+int pop(tNode** head){
+     tNode* temp=*head;
+     if (*head==NULL){
+         return 0;
+     }
+     *head=temp->next;
+     int data=temp->data;
+     free(temp);
+     return data;
+ }
